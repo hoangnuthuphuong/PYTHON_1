0315cc9f332095f7d3b06ff9d37e8d05ce868e51
HNTP <hoangnuthuphuong@gmail.com>
2022-01-18 18:14:54 +0700

    UML...PYTHON

--- Đồ án/test1.py
+++ Đồ án/test1.py
@@ -2,56 +2,62 @@ from Pythontest import Student
 
 
 class QuanLySinhVien:
-    dssv = []
+    listSinhVien = []
 
     def nhapSinhVien(self):
-        # Khởi tạo một sinh viên mới
-        name = input("Nhap ten sinh vien: ")
-        phone = int(input("Nhap so dien thoai sinh vien: "))
-        email = input("nhap email: ")
-        stuID = input("nhap ID sinh vien")
-        chuyennganh = str(input("nhap chuyen nganh: "))
-        hanhkiem = str(input("nhap hanh kiem: "))
+            # Khởi tạo một sinh viên mới
+            name = input("Nhap ten sinh vien: ")
+            phone = int(input("Nhap so dien thoai sinh vien: "))
+            email = input("nhap email: ")
+            stuID = input("nhap ID sinh vien")
+            chuyennganh = str(input("nhap chuyen nganh: "))
+            hanhkiem = str(input("nhap hanh kiem: "))
+
+            # Nhập dữ liệu cho sinh viên
+
+            def  __init__() -> object:
+                stu = (Student(name, phone, email, stuID, chuyennganh, hanhkiem))
+                self.dssv.append(stu)
 
 
-        # Nhập dữ liệu cho sinh viên
-        stu = (Student(name, phone, email, stuID, chuyennganh, hanhkiem ))
-        self.dssv.append(stu)
-
-
-    # Hàm hiển thị danh sách sinh viên ra màn hình 
+    # Hàm hiển thị danh sách sinh viên ra màn hình
     def showSinhVien(self, dssv):
-        # hien thi tieu de cot
-        print("{:<8} {:<18} {:<8}{:<8} {:<8} {:<8}"
-              .format("ID", "Name", "phone", "email", "chuyen nganh", "hanh kiem"))
-        # hien thi danh sach sinh vien
-        if (dssv.__len__() > 0):
-            for stu in dssv:
-                print("{:<8} {:<18} {:<8}{:<8} {:<8} {:<8}"
-                      .format(stu._studentID, stu._name, stu._phone, stu._mail, stu._chuyennganh, stu._hanhkiem))
-        print("\n")
-    # Hàm trả về danh sách sinh viên
+        def __init__(self):
+            # hien thi tieu de cot
+            print("{:<8} {:<18} {:<8}{:<8} {:<8} {:<8}"
+                  .format("ID", "Name", "phone", "email", "chuyen nganh", "hanh kiem"))
+            # hien thi danh sach sinh vien
+            def   __init__() -> object:
+
+                if (dssv.__len__() > 0):
+                    for stu in dssv:
+                       print("{:<8} {:<18} {:<8}{:<8} {:<8} {:<8}".format(stu._studentID, stu._name, stu._phone, stu._mail,
+                                                                       stu._chuyennganh, stu._hanhkiem))
+                print("\n")
+            # Hàm trả về danh sách sinh viên
+
+
+
     def getListSinhVien(self):
-        return self.dssv
-
-
-
-
+            return self.dssv
 
 
 
     def soLuongSinhVien(self):
-        return self.dssv.__len__()
+        return self.listSinhVien.__len__()
 
-    def generateID(self):
-        maxId = 1
+
+    # Hàm tìm kiếm sinh viên theo tên
+    # Trả về một danh sách sinh viên
+    def findByName(self, keyword):
+        listSV = []
         if (self.soLuongSinhVien() > 0):
-            maxId = self.dssv[0]._id
-            for stu in self.dssv:
-                if (maxId < stu._id):
-                    maxId = stu._id
-            maxId = maxId + 1
-        return maxId
+            for sv in self.listSinhVien:
+                if (keyword.upper() in sv._name.upper()):
+                    listSV.append(sv)
+        return listSV
+
+
 
     def updateSinhVien(self, ID):
         # Tìm kiếm sinh viên trong danh sách listSinhVien
@@ -78,33 +84,27 @@ class QuanLySinhVien:
     # Hàm xóa sinh viên theo ten
     muonxoa = []  # Khởi tạo một mảng chứa kết quả cần tìm
     name = input("Nhap ten sinh vien: ")  # Là chuỗi con bạn cần tìm, ví dụ 'abc'
-    for name in dssv:  # Duyệt từng phần tử trong mảng
-        if muonxoa in dssv:  # Xét xem chuỗi có nằm trong phần tử này hay không?
-            print(sorted(set(dssv), key=dssv.index))
-
-    # Hàm tìm kiếm sinh viên theo tên
-
-
+    for name in listSinhVien:  # Duyệt từng phần tử trong mảng
+        if muonxoa in listSinhVien:  # Xét xem chuỗi có nằm trong phần tử này hay không?
+            print(sorted(set(listSinhVien), key=listSinhVien.index))
 
 
     # Trả về một danh sách sinh viên
     def findByName(self, keyword):
         listSV = []
         if (self.soLuongSinhVien() > 0):
-            for sv in self.dssv:
+            for sv in self.listSinhVien:
                 if (keyword.upper() in sv._name.upper()):
                     listSV.append(sv)
         return listSV
 
-    # Hàm sắp xếp danh sach sinh vien theo điểm TB tăng dần
-
-
-
 
     # Hàm sắp xếp danh sach sinh vien theo tên tăng dần
     def sortByName(self):
-        self.dssv.sort(key=lambda x: x._name, reverse=False)
+        self.listSinhVien.sort(key=lambda x: x._name, reverse=False)
 
+    def getListSinhVien(self):
+        return self.listSinhVien
 
 
 
--- Đồ án/test2.py
+++ Đồ án/test2.py
@@ -6,13 +6,12 @@ while (1 == 1):
     print("\nCHUONG TRINH QUAN LY SINH VIEN C#")
     print("*************************MENU**************************")
     print("**  1. Them sinh vien.                               **")
-    print("**  2. Cap nhat thong tin sinh vien boi ID.          **")
-    print("**  3. Xoa sinh vien boi ten.                         **")
+    print("**  2. Cap nhat thong tin sinh vien.                 **")
+    print("**  3. Xoa sinh vien.                                **")
     print("**  4. Tim kiem sinh vien theo ten.                  **")
-    print("**  5. Sap xep sinh vien theo diem trung binh (GPA). **")
-    print("**  6. Sap xep sinh vien theo ten.                   **")
-    print("**  7. Hien thi danh sach giang vien                 **")
-    print("**  8. Hien thi danh sach sinh vien.                 **")
+    print("**  5. Sap xep sinh vien theo ten.                   **")
+    print("**  6. Hien thi danh sach giang vien                 **")
+    print("**  7. Hien thi danh sach sinh vien.                 **")
     print("**  0. Thoat                                         **")
     print("*******************************************************")
 
@@ -30,46 +29,59 @@ while (1 == 1):
         else:
             print("\nSanh sach sinh vien trong!")
     elif (key == 3):
-        if (dssv.soLuongSinhVien() > 0):
-            print("\n3. Xoa sinh vien.")
-
+        print("\n3. Xoa sinh vien.")
+        muonxoa = []  # Khởi tạo một mảng chứa kết quả cần tìm
+        name = input("Nhap ten sinh vien: ")  # Là chuỗi con bạn cần tìm, ví dụ 'abc'
+        for name in dssv:  # Duyệt từng phần tử trong mảng
+            if muonxoa in dssv:  # Xét xem chuỗi có nằm trong phần tử này hay không?
+                print(sorted(set(dssv), key=dssv.index))
 
 
 
     elif (key == 4):
-        if (dssv.soLuongSinhVien() > 0):
-            print("\n4. Tim kiem sinh vien theo ten.")
-            print("\nNhap ten de tim kiem: ")
-            name = input()
-            searchResult = dssv.findByName(name)
-            dssv.showSinhVien(searchResult)
+        print("\n4. Tim kiem sinh vien theo ten.")
+        print("\nNhap ten de tim kiem: ")
+        name = input()
+        if (name == 1):
+            if (dssv.soLuongSinhVien() > 0):
+                print("\n4. Tim kiem sinh vien theo ten.")
+                print("\nNhap ten de tim kiem: ")
+                name = input()
+                searchResult = dssv.findByName(name)
+                dssv.showSinhVien(searchResult)
+            else:
+                print("\nKhong tim thay trong danh sach")
         else:
-            print("\nSanh sach sinh vien trong!")
+            print("\nKhong tim thay trong danh sach")
+
+
+
+
     elif (key == 5):
-            print("\nSanh sach sinh vien trong!")
-
-
-    elif (key == 6):
         if (dssv.soLuongSinhVien() > 0):
-            print("\n6. Sap xep sinh vien theo ten.")
+            print("\n5. Sap xep sinh vien theo ten.")
             dssv.sortByName()
             dssv.showSinhVien(dssv.getListSinhVien())
         else:
             print("\nSanh sach sinh vien trong!")
-    elif (key == 7):
+
+
+    elif (key == 6):
         if (dssv.soLuongSinhVien() > 0):
             print("\n6. Sap xep sinh vien theo ID.")
             dssv.sortByName()
             dssv.showSinhVien(dssv.getListSinhVien())
         else:
             print("\nSanh sach sinh vien trong!")
-    elif (key == 8):
+    elif (key == 7):
         if (dssv.soLuongSinhVien() > 0):
             print("\n7. Hien thi danh sach sinh vien.")
             dssv.showSinhVien(dssv.getListSinhVien())
         else:
-            print("\nSanh sach sinh vien trong!")
+            print("\nSanh sach sinh vien trong 7!")
+
     elif (key == 0):
+
         print("\nBan da chon thoat chuong trinh!")
         break
     else:
